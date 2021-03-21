f23db30e2b4eed51e83819126933a5d43fc508ab
ehmicky <ehmicky@gmail.com>
2021-03-21 17:56:34 +0100

    Fix linting

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-magic-numbers */
+export const codepoints = [0x2588]
+/* eslint-enable no-magic-numbers */
+
+export const regexp = /.*/u
+
 // Note: this does not work with characters above U-ffff
 // However, no cross-platform-terminal-characters is currently above U-ffff
 // Otherwise `eval()` should be used instead.
@@ -5,8 +11,4 @@ const codepointToCharacter = function (codepoint) {
   return String.fromCharCode(codepoint)
 }
 
-export const regexp = /.*/u
-
-export const codepoints = [0x2588]
-
-export const characters = codepoints.map()
+export const characters = codepoints.map(codepointToCharacter)
