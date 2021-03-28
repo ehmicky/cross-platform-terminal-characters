579561a27dd60c6f3045f26ac1ef7d4a2c1db737
ehmicky <ehmicky@gmail.com>
2021-03-28 23:30:57 +0200

    Fix gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,9 @@ const gulpSharedTasks = require('@ehmicky/dev-tasks')
 
 gulpSharedTasks.buildRegister()
 
-const tasks = require('./src/main.js')
-// eslint-disable-next-line import/order
 const { print } = require('./gulp/print.js')
 
 module.exports = {
-  ...tasks,
+  ...gulpSharedTasks,
   print,
 }
