7572900dd9106d2ef0c583008c55f544dad2f630
ehmicky <ehmicky@gmail.com>
2021-06-21 16:59:15 +0200

    Fix gulpfile linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line import/no-unresolved, node/no-missing-import, import/no-unassigned-import
+// eslint-disable-next-line import/no-unassigned-import
 import '@ehmicky/dev-tasks/register.js'
 
-// eslint-disable-next-line import/no-unresolved, node/no-missing-import
 export * from '@ehmicky/dev-tasks'
 export { print } from './gulp/print.js'
