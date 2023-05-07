80d377cd5fcc0d8da2df1f097e0121c4c3b22e75
ehmicky <ehmicky@gmail.com>
2023-05-07 18:10:30 +0100

    Update lock file

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,10 +1,10 @@
-import { expectType } from 'tsd'
-
 import {
   codepoints,
   characters,
   regex,
 } from 'cross-platform-terminal-characters'
+import { expectType } from 'tsd'
+
 
 expectType<number>(codepoints[0]!)
 expectType<string>(characters[0]!)
--- src/main.test.js
+++ src/main.test.js
@@ -1,5 +1,4 @@
 import test from 'ava'
-
 import {
   codepoints,
   characters,
