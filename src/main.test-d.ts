1a357c7a86e7838cf978b33c9473419cc07ee6f2
ehmicky <ehmicky@gmail.com>
2023-05-13 02:12:23 +0100

    Fix linting

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -5,7 +5,6 @@ import {
 } from 'cross-platform-terminal-characters'
 import { expectType } from 'tsd'
 
-
 expectType<number>(codepoints[0]!)
 expectType<string>(characters[0]!)
 expectType<RegExp>(regex)
